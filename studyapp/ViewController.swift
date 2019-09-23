f288a9498995764ebe378e6b61c406772ffe4b37
Kye <kyemiller13@gmail.com>
2019-09-23 11:16:12 -0400

    Initial Commit

---
+++ studyapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studyapp
+//
+//  Created by Kye Miller on 9/23/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
