e88efc96c011b5c0b1399d13b93b263f51647ceb
Kye <kyemiller13@gmail.com>
2019-09-23 12:38:12 -0400

    setup connection with firebase, created compose view controller

---
+++ studyapp/ComposeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ComposeViewController.swift
+//  studyapp
+//
+//  Created by Kye Miller on 9/23/19.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addPost(_ sender: Any) {
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
