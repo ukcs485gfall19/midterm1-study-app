3a3b7214ec99bf81ef8efc506370852a28eb6a3d
jbach65 <joshbsteinbach@gmail.com>
2019-09-28 10:46:09 -0400

    plz just take my beautiful code

---
+++ messagingApp/ComposeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ComposeViewController.swift
+//  messagingApp
+//
+//  Created by Joshua Steinbach on 9/25/19.
+//  Copyright © 2019 Joshua Steinbach. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    var ref:DatabaseReference?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ref = Database.database().reference()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addPost(_ sender: Any) {
+        //add to database
+        if textView.text != "" {
+            ref?.child("Posts").childByAutoId().setValue(textView.text)
+
+            /*let id:String? = ref?.child("Posts").childByAutoId().key
+            ref?.child("Posts").child(id!).child("Body").setValue(textView.text) //not sure about this, Im forcibly unwrapping something that couls be nil. can it be nil?? idk...
+            ref?.child("Posts").child(id!).child("Title").setValue("Hi, I'm a title")*/
+        }
+        //close popup
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        //close popup
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ messagingApp/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  messagingApp
+//
+//  Created by Joshua Steinbach on 9/12/19.
+//  Copyright © 2019 Joshua Steinbach. All rights reserved.
+
+
+import UIKit
+import FirebaseDatabase
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var postData = [String]()
+    
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //set the firebase reference
+        ref = Database.database().reference()
+        
+        //retrieve posts and listen for changes
+        databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            let post = snapshot.value as? String
+            if let actualPost = post{
+                self.postData.append(actualPost)
+                self.tableView.reloadData()
+            }
+        })
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        
+        return cell!
+    }
+
+
+}
+
